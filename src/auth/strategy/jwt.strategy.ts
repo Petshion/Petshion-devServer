6269e5ee0574cb10471bfecd0be1d7c121fc7287
MurasakiPurki <chajuhan620@gmail.com>
2021-09-21 22:39:50 +0900

    fix: header

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -7,7 +7,7 @@ import { jwtConstants } from '../constants';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromAuthHeader(),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
     });
