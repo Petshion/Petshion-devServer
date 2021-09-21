d95b382f983cc40f4c69ecbaeccfe75b7440b5dd
MurasakiPurki <chajuhan620@gmail.com>
2021-09-21 18:29:38 +0900

    fix: queryDTO

--- src/product/dto/QueryFilteringProduct.dto.ts
+++ src/product/dto/QueryFilteringProduct.dto.ts
@@ -3,25 +3,25 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 export class QueryFilteringProductDto {
   @ApiPropertyOptional({
     description: 'breed of an animal',
-    type: [String],
+    type: String,
     required: true,
     nullable: false,
   })
-  breed?: string;
+  breed?: string[];
 
   @ApiPropertyOptional({
     description: 'color of an animal',
-    type: [String],
+    type: String,
     required: true,
     nullable: false,
   })
-  color?: string;
+  color?: string[];
 
   @ApiPropertyOptional({
     description: 'size of an animal',
-    type: [String],
+    type: String,
     required: true,
     nullable: false,
   })
-  size?: string;
+  size?: string[];
 }
