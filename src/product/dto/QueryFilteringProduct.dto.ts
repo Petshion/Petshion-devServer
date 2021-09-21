8c3f9a336c36f8d9202685d3ed3bc92350a2aba0
MurasakiPurki <chajuhan620@gmail.com>
2021-09-21 17:45:46 +0900

    fix: queryDto

--- src/product/dto/QueryFilteringProduct.dto.ts
+++ src/product/dto/QueryFilteringProduct.dto.ts
@@ -7,7 +7,7 @@ export class QueryFilteringProductDto {
     required: true,
     nullable: false,
   })
-  breed?: string;
+  breed?: any;
 
   @ApiPropertyOptional({
     description: 'color of an animal',
@@ -15,7 +15,7 @@ export class QueryFilteringProductDto {
     required: true,
     nullable: false,
   })
-  color?: string;
+  color?: any;
 
   @ApiPropertyOptional({
     description: 'size of an animal',
@@ -23,5 +23,5 @@ export class QueryFilteringProductDto {
     required: true,
     nullable: false,
   })
-  size?: string;
+  size?: any;
 }
